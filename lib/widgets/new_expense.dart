7bf4ae445061e4fecbf44938f6aee9bfb1ef2ce1
Firaol <loarifse16@gmail.comgit config --global user.email loarifse16@gmail.com>
2025-02-11 11:11:51 +0300

    cancel_function_added

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,46 +1,76 @@
 import 'package:flutter/material.dart';
 
-class NewExpense extends StatefulWidget{
+class NewExpense extends StatefulWidget {
   @override
   _NewExpenseState createState() => _NewExpenseState();
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
-final _titleController = TextEditingController();
-
-void dipose(){
-  _titleController.dispose();
-  super.dispose();
-}
-
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          TextField(
-          controller: _titleController,
           
-          maxLength:50,
-          decoration:const InputDecoration(
-            label:Text('Title'
-            )
-          )
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              labelText: 'Title',
+            ),
           ),
-         Row(children: [
-          ElevatedButton(
-            onPressed: () {
-              print(_titleController.text); 
-            },
-            child:Text('Save Expense')
-          )
-         ],)
-        ],
-        
 
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              labelText: 'Amount',
+            ),
+          ),
+
+          const SizedBox(height: 16), 
+        
+          Row(
+            children: [
+              const Text('Selected Date'),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.calendar_month),
+              ),
+            ],
+          ),
+
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
